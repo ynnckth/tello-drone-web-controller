e774701eec94dcf3368d12064a2d8bd2cb15d5e5
ynnckth <yannickstreit@hotmail.com>
2019-05-28 21:00:07 +0200

    Rounded corners for battery status component

--- frontend/src/components/battery-status/BatteryStatus.tsx
+++ frontend/src/components/battery-status/BatteryStatus.tsx
@@ -32,8 +32,8 @@ export default class BatteryStatus extends React.Component<IProps, IState> {
       <div className="battery-status">
         {this.props.batteryStatus &&
           <svg width='75' height='30'>
-            <rect width={this.BATTERY_MAX_WIDTH} height='25' fill='#666666' strokeWidth='1' stroke='#000'/>
-            <rect width={this.calculateBatteryBarWidth()} height='25' fill='#32CD32' strokeWidth='1' stroke='#000'/>
+            <rect width={this.BATTERY_MAX_WIDTH} height='25' fill='#666666' strokeWidth='1' stroke='#000' rx="2" ry="2"/>
+            <rect width={this.calculateBatteryBarWidth()} height='25' fill='#32CD32' strokeWidth='1' stroke='#000' rx="2" ry="2"/>
             <rect x="50" y="5" width='3' height='15' fill='#666666' strokeWidth='1' stroke='#000'/>
             <text x="12" y="17" fill="white" font-size="13">{this.props.batteryStatus}%</text>
           </svg>}
